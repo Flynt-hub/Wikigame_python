5dc661e2ea84cad3d46b4c8e18fd0b886ddecc72
Flynt-hub <73832334+Flynt-hub@users.noreply.github.com>
2021-03-04 11:03:22 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -4,8 +4,16 @@ from class_Scrapper import *
 
 if __name__ == "__main__" :
 
-    firstPage = "https://fr.wikipedia.org/wiki/Sp%C3%A9cial:Page_au_hasard"
-    goalPage  = "https://fr.wikipedia.org/wiki/Sp%C3%A9cial:Page_au_hasard"
+    if len(sys.argv) > 1 :
+        if sys.argv[1] == "debug" :
+            firstPage = "https://fr.wikipedia.org/wiki/Python_(langage)"
+            goalPage  = "https://fr.wikipedia.org/wiki/C_(langage)"
+        else : 
+            print("unknown argument..")
+            sys.exit()
+    else : 
+        firstPage = "https://fr.wikipedia.org/wiki/Sp%C3%A9cial:Page_au_hasard"
+        goalPage  = "https://fr.wikipedia.org/wiki/Sp%C3%A9cial:Page_au_hasard"
 
     scrapper   = Scrapper( firstPage, goalPage )
     gui        = GUI("1400x800")
